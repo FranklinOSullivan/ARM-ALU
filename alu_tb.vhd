a776240731d860e298a06883484f2a86f50fd7ef
Franklin O'Sullivan <franklino0808@gmail.com>
2024-03-04 17:26:16 +1300

    feat Added dut and primary test case

--- alu_tb.vhd
+++ alu_tb.vhd
@@ -18,4 +18,16 @@ architecture Behaviour of alu_tb is
     signal OP_TB : std_logic_vector(2 downto 0);
 
 begin
+    dut: alu port map (I_A => A_TB, I_B => B_TB, I_OP => OP_TB, O_Result => Result_TB, O_Zero => Zero_TB);
+
+    -- Test cases
+    process
+    begin
+        -- Test 1: AND
+        A_TB <= '0', B_TB <= '0', OP_TB <= "000";
+        wait for 10 ns;
+        assert Result_TB = '0' and Zero_TB = '0' report "Test case 1 Passes / Failed"
+        
+        wait;
+    end process;
 end Behaviour;
